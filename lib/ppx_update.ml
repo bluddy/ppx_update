afbce53782251f8ecd99732f6f4c005ebc91a0e1
Yotam Barnoy <yotambarnoy@gmail.com>
2023-08-06 21:53:55 +0300

    update syntax extension for records

--- lib/ppx_update.ml
+++ lib/ppx_update.ml
@@ -1,6 +1,29 @@
 open Ppxlib
+open Ppxlib.Ast_builder.Default
 
 let name = "ppx_update"
+
+(* if b.a == a && b.c == c then b else {b with a; c} *)
+let create_test ~loc elist rec_expr =
+  let rec loop elist =
+    let create_comp ~loc lident comp_exp =
+      (eapply ~loc
+        (evar ~loc "==") @@
+        [pexp_field ~loc rec_expr lident; comp_exp])
+    in
+    match elist with
+    | [lident, e] ->
+        create_comp ~loc lident e
+    | (lident, e)::rest ->
+        eapply ~loc
+          (evar ~loc "&&")
+          [create_comp ~loc lident e; loop rest]
+    | [] -> assert false
+  in
+  loop elist
+
+let create_record ~loc elist rec_expr =
+  pexp_record ~loc elist (Some rec_expr)
 
 let record_extension name =
   Extension.declare name
@@ -11,7 +34,11 @@ let record_extension name =
           (many __)
           (some __)
       )
-    (fun ~loc:_ ~path:_ _a b -> b)
+    (fun ~loc ~path:_ elist rec_expr ->
+      pexp_ifthenelse ~loc
+        (create_test ~loc elist rec_expr)
+        rec_expr
+        (Some (create_record ~loc elist rec_expr)))
 
 let () = Driver.register_transformation  name
   ~rules:
--- test/test.ml
+++ test/test.ml
@@ -3,17 +3,22 @@ type a = {
   x: int;
 } [@@deriving show]
 
+type d = {
+  y: int;
+} [@@deriving show]
+
 type b = {
   a: a;
+  d: d;
   foo: int;
 } [@@deriving show]
 
 let () =
   let a = {x=3} in
-  (* let a2 = {x=5} in *)
+  let d = {y=1} in
 
-  let b = {a=a; foo=10} in
-  let b2 = [%update {b with a}] in
+  let b = {a=a; d=d; foo=10} in
+  let b2 = [%update {b with a; d}] in
   print_string @@ show_b b2
   
 
